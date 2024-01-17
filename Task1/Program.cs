ceb743a7f9ba3498101f89ffdb4a0ea962bab8a2
Tsolak <dcntsolak@yahoo.com>
2024-01-17 11:54:07 +0400

    Added Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задача 1: Напишите программу,
+//которая принимает на вход число и проверяет,
+//кратно ли оно одновременно 7 и 23.
+
+/*void MultipleOfNumber(int num)
+{
+    if (num % 7 == 0 && num % 23 == 0)
+    {
+        Console.WriteLine($"да, {num} кратно");
+    }
+    else
+    {
+        Console.WriteLine($"нет, {num} не кратно");
+    }
+}
+
+Console.WriteLine("Input number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+MultipleOfNumber(num);*/
+
+
