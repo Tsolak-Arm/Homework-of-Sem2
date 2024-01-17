1f8edf2d297181605bf50e1913a6b6d25230cb5b
Tsolak <dcntsolak@yahoo.com>
2024-01-17 15:48:23 +0400

    Added Homework_of_Sem2:Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задача 3: Напишите программу, которая
+//принимает на вход целое число из отрезка [10, 99]
+//и показывает наибольшую цифру числа.
+
+void GetMaxDigit()
+{
+    Console.Write("Введите целое число от 10 до 99: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+
+    if (number >= 10 && number <= 99)
+    {
+        int maxDigit = Math.Max(number / 10, number % 10);
+        Console.WriteLine($"Наибольшая цифра числа {number}: {maxDigit}");
+    }
+    else
+    {
+        Console.WriteLine("Некорректный ввод. Введите целое число от 10 до 99.");
+    }
+}
+GetMaxDigit();
