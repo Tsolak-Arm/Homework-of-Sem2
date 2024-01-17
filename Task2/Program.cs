05f0dca1d0a19c8bf26414191e2fc0b25ad80e5c
Tsolak <dcntsolak@yahoo.com>
2024-01-17 13:33:34 +0400

    Added Homework_of_Sem2:Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,39 @@
+﻿//Задача 2: Напишите программу, которая принимает на вход координаты точки (X и Y),
+//причём X ≠ 0 и Y ≠ 0 и выдаёт номер координатной четверти плоскости,
+//в которой находится эта точка.
+
+using System;
+
+void GetNumOfCoordQuad()
+{
+    // Ввод координат X и Y
+    Console.Write("X: ");
+    double x = double.Parse(Console.ReadLine());
+
+    Console.Write("Y: ");
+    double y = double.Parse(Console.ReadLine());
+
+    // Определение номера координатной четверти
+    int quadrant = DetermineQuad(x, y);
+
+    // Вывод результата
+    Console.WriteLine($"Точка находится в {quadrant}-й координатной четверти.");
+}
+
+int DetermineQuad(double x, double y)
+{
+    if (x > 0 && y > 0)
+        return 1;
+    else if (x < 0 && y > 0)
+        return 2;
+    else if (x < 0 && y < 0)
+        return 3;
+    else if (x > 0 && y < 0)
+        return 4;
+    else
+    // Если точка не принадлежит ни одной координатной четверти, возвращаем 0
+    return 0;
+}
+
+// Точка входа
+GetNumOfCoordQuad();
